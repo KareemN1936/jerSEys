cc85cbe7acab0bac300cb1c94247ac4daef21b71
Kareem Naous <62202089+KareemN1936@users.noreply.github.com>
2026-06-06 03:40:05 +0300

    FIx Aiven MySQL connection

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,17 +1,38 @@
 const mysql = require("mysql2/promise");
 
-const pool = process.env.DATABASE_URL
-  ? mysql.createPool(process.env.DATABASE_URL)
-  : mysql.createPool({
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      waitForConnections: true,
-      connectionLimit: 10,
-      queueLimit: 0
-    });
+let pool;
+
+if (process.env.DATABASE_URL) {
+  const dbUrl = new URL(process.env.DATABASE_URL);
+
+  pool = mysql.createPool({
+    host: dbUrl.hostname,
+    port: Number(dbUrl.port),
+    user: dbUrl.username,
+    password: decodeURIComponent(dbUrl.password),
+    database: dbUrl.pathname.replace("/", ""),
+    ssl: {
+      rejectUnauthorized: false
+    },
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+  });
+} else {
+  pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT || 3306),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    ssl: {
+      rejectUnauthorized: false
+    },
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+  });
+}
 
 const testDbConnection = async () => {
   const connection = await pool.getConnection();
